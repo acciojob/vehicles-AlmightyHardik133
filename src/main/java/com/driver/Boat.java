ae540ec090a7127a27d802377785644bedbf1f01
mehta <mehta@LAPTOP-B1LKLJTG>
2023-10-30 17:24:18 +0530

    Third

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -11,6 +11,26 @@ public class Boat implements WaterVehicle{
 	}
 	
 	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+
 	@Override
 	public String getVehicleName() {
 		// TODO Auto-generated method stub
