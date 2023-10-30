dcc1c53318e4b73159bcb464223c1e90f54f5abe
mehta <mehta@LAPTOP-B1LKLJTG>
2023-10-30 17:22:06 +0530

    second commit

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,26 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+	
+	private String name;
+	private int capacity;
+	
+	public Boat(String name, int capacity) {
+		this.name = name;
+		this.capacity = capacity;
+	}
+	
+	
+	@Override
+	public String getVehicleName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	@Override
+	public int getVehicleCapacity() {
+		// TODO Auto-generated method stub
+		return this.capacity;
+	}
+
+}
